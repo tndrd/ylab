d98a038007e50814d8aa095c90063aba196d7855
Vladimir Lekhterev <vovalekhterev@gmail.com>
2022-11-24 20:33:10 +0300

    Line3D is now a parent

--- HW3D/include/Line3D.hpp
+++ HW3D/include/Line3D.hpp
@@ -1,102 +1,54 @@
+#pragma once
+
 #include "Vec3D.hpp" 
 
 namespace HW3D
 {
+
 class Line3D
 {
+  // Parent class for 3D lines
+  
+  protected:
   // Line equation: V = p + a*t
   Vec3D   a_; // Direction vector.
   Point3D p_; // Line offset radius-vector.
 
+  // a and p are class invariants, so they should be protected
+  // Line3D will have two childs: line segment LineSeg3D, infinite line LineInf3D, ray LineRay3D
+  // They construct differently: 
+  //  - LineSeg3D from two points
+  //  - LineInf3D as an intersection of two planes or by explicitly given a and p
+  //  - LineRay3D from direction a and point p
+  // Defining a and p correctly is crucial for LineSeg3D, 
+  // invalid definition will break line segment intersection algorithm
+  // So there is no need for user to change a or p
+
+  // The constructor is protected too
+  // User needs to create one of childs to create a line
+
+  Line3D(const Vec3D& a, const Point3D& p): a_(a), p_(p) {}
+
   public:
 
-  Line3D(const Point3D& p1, const Point3D& p2): a_(p2 - p1), p_(p1) {}
-
-  Vec3D   get_a() const { return a_; } // Getters 
-  Point3D get_p() const { return p_; } //
+  Vec3D   get_a() const {return a_;}
+  Point3D get_p() const {return p_;}
 
   Point3D point_from_param(double t) const
   {
     return p_ + a_ * t;
   }
+
+  bool is_parallel(const Line3D& l2) const
+  {
+    return abs(a_.normalize()) == abs(l2.a_.normalize());
+  }
+
+  // Coincidence of childs determines differently for each child
+  virtual bool is_coincident(const Line3D&) const;
+
+  // Check if given parameter fits the restrictions on t
+  virtual bool check_param(double t) const;  
 };
-
-struct Line2LineRelation
-{
-  enum State
-  {
-    PARALLEL,
-    COINCIDENT,
-    INTERSECTING,
-    NON_INTERSECTING,
-  } state;
-
-  // Let the distance vector between l1 and l2 be Q2 - Q1
-  // Q1 = p1 + a1 * t1
-  // Q2 = p2 + a2 * t2
-
-  double t1  = NAN; 
-  double t2 = NAN;
-};
-
-bool parallel(const Line3D& l1, const Line3D& l2)
-{
-  return abs(l1.get_a()) == abs(l2.get_a());
-}
-
-bool coincident(const Line3D& l1, const Line3D& l2)
-{
-  return parallel(l1, l2) && (l1.get_p() == l2.get_p());
-}
-
-// Reference: GCT page 409
-Line2LineRelation get_lines_relation(const Line3D& l1, const Line3D& l2)
-{
-
-  Line2LineRelation relation;
-
-  if (parallel(l1, l2))
-  {
-    relation.state = Line2LineRelation::State::PARALLEL;
-    return relation;
-  }
-
-  if (coincident(l1, l2))
-  {
-    relation.state = Line2LineRelation::State::COINCIDENT;
-    return relation;
-  }
-
-  Vec3D   a1 = l1.get_a();
-  Point3D p1 = l1.get_p();
-
-  Vec3D   a2 = l2.get_a();
-  Point3D p2 = l2.get_p();
-
-  Vec3D  u = p1 - p2;
-  double A = a1 * a1;
-  double B = - a1 * a2;
-  double C = a2 * a2;
-  double D = a1 * u;
-  double E = a2 * u;
-  double F = u * u;
-
-  relation.t1 = (A*E - C*D) / (A*C - B*B);
-  relation.t2 = (B*D - A*E) / (A*C - B*B);
-
-  Point3D Q1 = l1.point_from_param(relation.t1);
-  Point3D Q2 = l2.point_from_param(relation.t2);
-
-  if (fit((Q2 - Q1).length(), 0))
-  {
-    relation.state = Line2LineRelation::State::INTERSECTING;
-  }
-  else
-  {
-    relation.state = Line2LineRelation::State::NON_INTERSECTING;
-  }
-
-  return relation;
-}
 
 }
