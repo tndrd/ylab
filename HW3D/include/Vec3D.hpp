49de776adda0d7ca2fc17bdb04833995fd787a2c
Vladimir Lekhterev <vovalekhterev@gmail.com>
2022-11-24 12:48:13 +0300

    overload and rename

--- HW3D/include/Vec3D.hpp
+++ HW3D/include/Vec3D.hpp
@@ -6,6 +6,11 @@ namespace HW3D
 
 const double FIT_TOLERANCE = 0.00001;
 
+bool fit(double a, double b)
+{
+  return (a - b) < FIT_TOLERANCE;
+}
+
 struct Vec3D
 {
   double x = NAN;
@@ -31,14 +36,14 @@ struct Vec3D
     z = vec.z;
   }
 
-  double abs() const
+  double length() const
   {
     return std::sqrt(x*x + y*y + z*z);
   }
 
   void normalize()
   {
-    double absval = abs();
+    double absval = length();
 
     x /= absval;
     y /= absval;
@@ -81,9 +86,24 @@ Vec3D operator/(const double k, const Vec3D& v)
   return {v.x / k, v.y / k, v.z / k};
 }
 
+Vec3D operator-(const Vec3D& v)
+{
+  return {-v.x, -v.y, -v.z};
+}
+
 Vec3D project_v(const Vec3D& which_vec, const Vec3D& where_vec)
 {
-  return (which_vec * where_vec / where_vec.abs()) * where_vec; 
+  return (which_vec * where_vec / where_vec.length()) * where_vec; 
+}
+
+Vec3D abs(const Vec3D& v)
+{
+  return {std::abs(v.x), std::abs(v.y), std::abs(v.z)};
+}
+
+bool operator==(const Vec3D& v1, const Vec3D& v2)
+{
+  return fit(v1.x, v2.x) && fit(v1.y, v2.y) && fit(v1.z, v2.z);
 }
 
 using Point3D = Vec3D; // Just for aestethical purposes. Just to emphasise that Point3D is a radius-vector
