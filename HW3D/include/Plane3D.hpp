7b97624086e18598163a240d1aebe0f475fb07ec
Vladimir Lekhterev <vovalekhterev@gmail.com>
2022-11-26 15:34:52 +0300

    Fix normalization and coincidence check

--- HW3D/include/Plane3D.hpp
+++ HW3D/include/Plane3D.hpp
@@ -8,6 +8,12 @@ namespace HW3D
 
 class Plane3D
 {
+
+  // Plane equation: (n, P) = s
+  // n - plane normal, |n| = 1
+  // s - plane offset
+  // Equation should be tranformed into one with s >= 0 
+
   Vec3D  n_;
   double s_;
 
@@ -21,11 +27,20 @@ class Plane3D
     return det(c, b, a);
   }
 
+  // Normalization is an inportant part of defining planes in 3D
+  // After the normalization parameters and their planes become one-to-one correspondent 
   void normalize()
   {
     double nlen = n_.length();
-    n_ = n_ / nlen;
-    s_ = s_ / nlen; 
+
+    double ssgn = 1;
+    if (s_ != 0)
+    {
+      ssgn = s_ / std::abs(s_);
+    }
+
+    n_ = n_ / nlen * ssgn;
+    s_ = s_ / nlen * ssgn; 
   }
 
   public:
@@ -51,12 +66,15 @@ class Plane3D
     return s_;
   }
 
+  // Because of normalization planes wont be equal if their s-params are not same
   bool is_coincident(const Plane3D& p) const
-  {
-    bool cond1 = (p.n_ == n_) && (p.s_ == s_);
-    bool cond2 = (p.n_ == - n_) && (p.s_ == - s_);
-
-    return cond1 || cond2;
+  { 
+    if (fit(p.s_, s_))
+    {
+      if (!fit(s_, 0)) return (p.n_ == n_);
+      else             return abseq(p.n_, n_);
+    }
+    return false;
   }
 
   bool is_parallel(const Plane3D& p) const
