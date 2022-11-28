782d9856b8b42699a22173ff8db548d20377b49b
Vladimir Lekhterev <vovalekhterev@gmail.com>
2022-11-28 15:46:15 +0300

    Fix complanar triangles intersection algorithm

--- HW3D/include/task_algorithm.hpp
+++ HW3D/include/task_algorithm.hpp
@@ -28,6 +28,21 @@ bool intersect_complanar_triangles(const Triangle3D& tr1, const Triangle3D& tr2)
       return false;
   }
 
+  for (int i = 0; i < 3; i++)
+  {
+    auto interval1 = ComputeInterval(tr1, normals2[i]);
+    auto interval2 = ComputeInterval(tr1, normals2[i]);
+
+    double max1 = interval1[1];
+    double min1 = interval1[0];
+
+    double max2 = interval2[1];
+    double min2 = interval2[0];
+
+    if (!intervals_intersect(min1, max1, min2, max2))
+      return false;
+  }
+
   return true;
 }
 
