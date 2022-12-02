92406c5523bb6fd3341634c9a49f56e8e8bb2096
Vladimir Lekhterev <vovalekhterev@gmail.com>
2022-12-03 00:51:44 +0300

    Another sample main

--- Matrix/src/main.cc
+++ Matrix/src/main.cc
@@ -5,9 +5,6 @@ using namespace HWMatrix;
 int main()
 {
   std::vector<double> values {1.5, 0, 0, 0, 0, 0, 0, 0, 0};
-  auto mat = Matrix<double, 3, 3>{values}.swap_rows(1,2);
-
-  //mat.gugugaga();
-
+  Matrix<double, 3, 3> mat {values};
   std::cout << mat;
 }
