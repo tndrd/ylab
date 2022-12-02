2ce01eae3682762b0570d4e7b10eb76303f48840
Vladimir Lekhterev <vovalekhterev@gmail.com>
2022-12-02 17:33:45 +0300

    Add blank unit tests file

---
+++ Matrix/src/utests.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include "matrix.hpp"
+
+using namespace HWMatrix;
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
