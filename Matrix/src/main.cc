2420e7d002b05608988998bfe31fdca149fa8624
LilPetia <kuznetsov.nikolai@phystech.edu>
2022-12-03 18:57:37 +0300

    Add main driver

--- Matrix/src/main.cc
+++ Matrix/src/main.cc
@@ -2,9 +2,30 @@
 
 using namespace HWMatrix; 
 
+
+Matrix<double> read_matrix()
+{
+  size_t dim;
+  std::cin >> dim;
+  assert(std::cin.good());
+
+  size_t size = dim * dim;
+  std::vector<double> data;
+
+  double val;
+  for(int i = 0; i < size; ++i)
+  {
+    std::cin >> val;
+    assert(std::cin.good());
+    data.push_back(val);
+  }
+
+  return {dim, dim, data};
+}
+
 int main()
 {
-  std::vector<double> values {1.5, 0, 0, 0, 0, 0, 0, 0, 0, 1};
-  Matrix<double> mat {5, 2, values};
-  std::cout << mat;
+  Matrix<double> mat = read_matrix();
+  std::cout << det(mat) << std::endl;
+  return 0;
 }
