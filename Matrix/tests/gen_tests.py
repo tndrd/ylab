d84a2829c0c7a7d31cc3a50e94f80b921177605e
Vladimir Lekhterev <vovalekhterev@gmail.com>
2022-12-21 15:53:59 +0300

    Add degenerate matrix tests and rebuild tests

--- Matrix/tests/gen_tests.py
+++ Matrix/tests/gen_tests.py
@@ -1,14 +1,22 @@
 from random import randint
-
+from math import log
 MAXSIZE  = 100
 MAXDET   = 100
 
 def gen(quantity):
   tests = []
-
+  print(f"Generating {quantity} scrambled matrices")
   for i in range(quantity):
     size = randint(1, MAXSIZE)
     det  = randint(-MAXDET, MAXDET)
+    test = f"{size} {det}\n"
+    tests.append(test)
+
+  zerodet_quantity = int(log(quantity)) + 5 
+  print(f"Generating {zerodet_quantity} scrambled degenerate matrices")
+  for i in range(zerodet_quantity):
+    size = randint(1, MAXSIZE)
+    det  = 0
     test = f"{size} {det}\n"
     tests.append(test)
 
