dc945d6a6abfa3d109c7072c21b0891c6a3f074a
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-01-07 17:08:34 +0300

    Add e2e test generation & tune

--- HW3D/tests/testgen.py
+++ HW3D/tests/testgen.py
@@ -5,11 +5,12 @@ import numpy as np
 
 N_SCRAMBLES = 10
 
-X_STEP = (pairgen.RBORDER - pairgen.LBORDER) * 2
-Y_STEP = (pairgen.TBORDER - pairgen.BBORDER) * 2
-Z_STEP = (pairgen.ZBORDER_T - pairgen.ZBORDER_B) * 2
+X_STEP = (pairgen.RBORDER - pairgen.LBORDER) * 20
+Y_STEP = (pairgen.TBORDER - pairgen.BBORDER) * 20
+Z_STEP = (pairgen.ZBORDER_T - pairgen.ZBORDER_B) * 20
 
-TESTFILE = "triangles.test"
+UNIT_TEST_FILE = "triangles.test"
+E2E_TEST_FILE  = "e2e.test"
 
 def dump_triangle(tests, tr):
   for i in range(len(tr)):
@@ -17,10 +18,9 @@ def dump_triangle(tests, tr):
       tests = tests + f'{tr[i][k]} '
   return tests
 
-def dump_test(tests, intersection, tr1, tr2):
+def dump_test(tests, tr1, tr2):
   tests = dump_triangle(tests, tr1)
   tests = dump_triangle(tests, tr2)
-  tests = tests + f'{int(intersection)}\n'
   return tests
 
 def pair():
@@ -45,20 +45,46 @@ def offset(tr1, tr2, x, y, z):
 
 def generate_tests(x_n, y_n, z_n):
   test_n = 0
-  tests = ""
+  intersects = 0
+  
+  unit_tests = ""
+  e2e_tests  = ""
+  e2e_result = []
+  
   for z in tqdm(range(-z_n, z_n)):
     for y in tqdm(range(-y_n, y_n), leave=False):
       for x in range(-x_n, x_n):
         intersection, tr1, tr2 = pair()
         tr1, tr2 = offset(tr1, tr2, x, y, z)
-        tests = dump_test(tests, intersection, tr1, tr2)
+        
+        unit_tests = dump_test(unit_tests, tr1, tr2)
+        unit_tests = unit_tests + f'{int(intersection)}\n'
+        
+        e2e_tests = dump_test(e2e_tests, tr1, tr2)
+        if intersection:
+          e2e_result.append(2*test_n)
+          e2e_result.append(2*test_n + 1)
+          intersects = intersects + 2
+
         test_n = test_n + 1
 
-  return test_n, tests
+  e2e_result.sort()
+
+  e2e_result_string = ""
+  for x in e2e_result: 
+    e2e_result_string = e2e_result_string + f'{x} ' 
+  
+  return test_n, intersects, unit_tests, e2e_tests, e2e_result_string
 
 if __name__ == "__main__":
-  with open(TESTFILE, "w") as f:
-    x_n, y_n, z_n = map(int, input("Enter dimensions: ").split())
-    test_n, tests = generate_tests(x_n, y_n, z_n)
-    f.write(f'{test_n}\n' + tests)
+  x_n, y_n, z_n = map(int, input("Enter dimensions: ").split())
+  test_n, intersects, unit_tests, e2e_tests, e2e_result = generate_tests(x_n, y_n, z_n)
+  
+  with open(UNIT_TEST_FILE, "w") as f:
+    f.write(f'{test_n}\n' + unit_tests)
+
+  with open(E2E_TEST_FILE, "w") as f:
+    f.write(f'{test_n * 2}\n' + e2e_tests + f'\n{intersects}\n' + e2e_result)
+
   print(f"Successfuly generated {test_n} triangle pairs")
+  print(f"Number of intersections: {intersects}")
