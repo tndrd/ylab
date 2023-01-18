dec809768de94ebf73c8c70cc9fb35310d941c9b
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-01-18 23:27:25 +0300

    Refactor

--- HW3D/include/triangle_intersections.hpp
+++ HW3D/include/triangle_intersections.hpp
@@ -6,11 +6,11 @@
 
 namespace HW3D
 {
-std::array<double,2> ComputeInterval(const Triangle3D& tr, const Vec3D& direction) noexcept;
+std::array<double,2> ComputeInterval(const Triangle3D& tr, const Vec3D& direction);
 
-bool intersect_complanar_triangles(const Triangle3D& tr1, const Triangle3D& tr2) noexcept;
+bool intersect_complanar_triangles(const Triangle3D& tr1, const Triangle3D& tr2);
 
-bool intersect_noncomplanar_triangles(const Triangle3D& tr1, const Triangle3D& tr2) noexcept;
+bool intersect_noncomplanar_triangles(const Triangle3D& tr1, const Triangle3D& tr2);
 
-bool intersect_triangles(const Triangle3D& tr1, const Triangle3D& tr2) noexcept;
+bool intersect_triangles(const Triangle3D& tr1, const Triangle3D& tr2);
 }
