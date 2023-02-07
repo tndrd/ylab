d5a37539e79485f5bf8b15d5ab28a020e95d7871
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-02-07 16:57:47 +0300

    Make error output less violent

--- Matrix/include/task.hpp
+++ Matrix/include/task.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "matrix_algorithm.hpp"
-#include <cassert>
+#include <cctype>
 
 namespace HWMatrix {
 namespace Task {
@@ -10,18 +10,26 @@ namespace Task {
 template<typename T>
 Matrix<T> read_matrix(std::istream& input)
 {
-  size_t dim;
-  input >> dim;
-  assert(input.good());
+  int dim_input;
+  input >> dim_input;
+  if (!input.good()) throw std::invalid_argument("Read error");
 
+  if (dim_input <= 0)
+    throw std::invalid_argument("Can't create matrix with incorrect dimensions");
+
+  size_t dim  = static_cast<size_t>(dim_input);
   size_t size = dim * dim;
   std::vector<T> data;
 
-  T val;
   for(int i = 0; i < size; ++i)
   {
+    char sep = input.get();
+    if (!std::isspace(sep))
+      throw std::invalid_argument("Format error");
+    
+    T val;
     input >> val;
-    assert(input.good());
+    if (!input.good()) throw std::invalid_argument("Read error");
     data.push_back(val);
   }
 
