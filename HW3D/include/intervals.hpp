55f53bc528a09fbf6c95a46d35476af217a35418
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-02-16 15:40:11 +0300

    Add comparison funcs

--- HW3D/include/intervals.hpp
+++ HW3D/include/intervals.hpp
@@ -14,27 +14,34 @@ inline bool fit(double a, double b) noexcept
   return std::abs(a - b) < FIT_TOLERANCE;
 }
 
+inline bool greater(double a, double b)
+{
+  return a > b + FIT_TOLERANCE;
+}
+
+inline bool lesser(double a, double b)
+{
+  return a < b - FIT_TOLERANCE;
+}
+
+inline bool egreater(double a, double b)
+{
+  return a >= b - FIT_TOLERANCE;
+}
+
+inline bool elesser(double a, double b)
+{
+  return a <= b + FIT_TOLERANCE;
+}
+
 // a <= t <= b
 inline bool interval_fit(double t, double a, double b) noexcept
 {
-  bool cond1 = (t >= a) && (t <= b);
-  bool cond2 = fit(t, a);
-  bool cond3 = fit(t, b);
-  return cond1 || cond2 || cond3;
+  return egreater(t, a) && elesser(t, b);
 }
 
 inline bool intervals_intersect(double min1, double max1, double min2, double max2) noexcept
 {
-  if(fit(max1,min2) || fit(max2, min1))
-  {
-    return true;
-  }
-
-  if (max1 < min2 || max2 < min1)
-  {
-    return false;
-  }
-
-  return true;
+  return egreater(max1, min2) && elesser(min1, max2);
 }
 }
