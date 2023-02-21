4d8dd2f0cd5773c7958026ce62b86aa5cd2fbcb2
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-02-21 17:28:54 +0300

    Add performance comparison tests

--- Caches/src/test.cc
+++ Caches/src/test.cc
@@ -6,34 +6,48 @@
 
 using namespace HWCache;
 
+// -------------- GENERAL -----------------------------
+
 TEST(LRU, E2E_hardcoded)
 {
-  test_cache<LRU<int>>("tests/LRU.test");
+  test_cache<LRU<int>>("tests/build/LRU.test");
 }
 
 TEST(LRU, E2E_gen)
 {
-  test_cache<LRU<int>>("tests/LRU_gen.test");
+  test_cache<LRU<int>>("tests/build/LRU_gen.test");
 }
 
 TEST(LFU, E2E_hardcoded)
 {
-  test_cache<LFU<int>>("tests/LFU.test");
+  test_cache<LFU<int>>("tests/build/LFU.test");
 }
 
 TEST(LFU, E2E_gen)
 {
-  test_cache<LFU<int>>("tests/LFU_gen.test");
+  test_cache<LFU<int>>("tests/build/LFU_gen.test");
 }
 
 TEST(ideal, E2E_hardcoded)
 {
-  test_cache<ideal<int>>("tests/ideal.test");
+  test_cache<ideal<int>>("tests/build/ideal.test");
 }
 
 TEST(ideal, E2E_gen)
 {
-  test_cache<ideal<int>>("tests/ideal_gen.test");
+  test_cache<ideal<int>>("tests/build/ideal_gen.test");
+}
+
+// -------------- IDEAL PERFORMANCE ----------------------
+
+TEST(ideal, E2E_perf)
+{
+  test_cache<ideal<int>>("tests/build/ideal_perf.test");
+}
+
+TEST(ideal_old, E2E_perf)
+{
+  test_cache<ideal_old<int>>("tests/build/ideal_perf.test");
 }
 
 int main(int argc, char **argv) {
